53718e985c372cf08db95ee22cb55901e44624a2
Mitsuharu Goto <mh.goto.web@gmail.com>
2023-11-09 01:22:56 +0900

    verson up

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const { updateElectronApp } = require("update-electron-app");
+updateElectronApp();
+
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("node:path");
 
